5bb29fcd529726d6ee0e1f498ec0c81b3eb30bdb
Wouter van Ooijen <wouter@voti.nl>
2021-07-25 08:46:10 +0200

    update

---
+++ _update.bat
@@ -0,0 +1,7 @@
+rem https://stackoverflow.com/questions/23825317/how-to-convert-markdown-css-pdf
+
+c:\python38\python.exe -m grip readme.md --export cv.html
+git add -A
+git commit -a -m "update"
+git push
+pause 
